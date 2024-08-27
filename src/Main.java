688b6b3434f2ae03d8a791006ed9de16b3d0acf6
Joseph-ui-sketch <hernajos000@warren.k12.in.us>
2024-08-27 13:10:41 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String []args){
+        printEqual(1,1,1);
+        printEqual(1,1,2);
+        printEqual(-1,-1,-1);
+        printEqual(1,2,3);
+    }
+
+    private static void printEqual(int num1,int num2, int num3){
+
+        if (num1 < 0);
+        if (num2 < 0);
+        if (num3 < 0);
+
+        if (num1 == num2);
+        if (num2 ==num3);
+
+
+        if ( num1!=num2);
+        if (num1 != num3);
+
+    }
+
+}
